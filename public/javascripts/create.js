e8061f05942146b9dc41661eb1405c7056c53fdb
Anthony Zhang <azhang@hugeinc.com>
2015-03-11 02:38:50 -0400

    started version 2.0. everything is being redone.

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,282 +1,194 @@
-var totalBags;
-var dataText = null;
-var dataY = 0;
-var dataSize = "small";
-var dataColor = "black";
-var dataJustify = "left";
-var dataFontSize = 5;           //em
-var dataKerning = -0.03;        //em
-var dataLeading = 1;            //em
-
-// show when a tool is selected  + focus it(textarea/image)
-function selectTool($tool){
-    deselectAll();
-    $tool.addClass('active');
-    if ($tool.hasClass("text-wrap")){
-        $tool.find('textarea').attr("readonly", false);
-        $tool.find('textarea').focus();
-    }
-}
-// deselect a selected tool (textarea/image)
-function deselectTool($tool){
-    $tool.removeClass("active");
-    if ($tool.hasClass("text-wrap")){
-        $tool.find('textarea').attr("readonly", true);
-    }
-}
-//deselect all tools
-function deselectAll(){
-    $(".tool").each(function(){
-        deselectTool($(this));
-    });
-}
-//manage textarea sizing based on clone
-function updateTextAreaSize(){
-    var content = $(".text-wrap textarea").val();
-    
-    // Size to be saved.
-    dataText = content;
-
-    if (content === ""){
-        content = "Type Something.";
-        dataText = null; // don't count it if its empty
-    }
-    //content = content.replace(/\n/g, '<br>');
-    $(".clone-text").html(content);
-
-    TweenLite.to(".text-wrap", 0.1, { height : $(".clone-text").height() + 10 } );
-
-    var amountMoved = Number($(".text-wrap").attr("data-y"));
-    if ((amountMoved + $(".text-wrap").height()) > ($(".tote-wrap").height() * 0.4)){
-        console.log('it happened');
-    }
-}
-
-// loads all the bags into array
-function loadBags(){
-    $.getJSON('/totes', function( data ){
-        totalBags = data.length;
-    });
-}
-
-// ADD TOTE BAG TO DATABASE
-function saveTote(){
-    var newTote = {
-        'text' : dataText,
-        'yAxis' : dataY,
-        'size' : dataSize,
-        'color' : dataColor,
-        'justification' : dataJustify,
-        'fontSize' : dataFontSize,
-        'kerning' : dataKerning,
-        'leading' : dataLeading,
-        'likes' : 0,
-        'timestamp' : Math.round(new Date().getTime() / 1000),
-        'bagNum' : totalBags+1
-    };
-
-    // use ajax to post tote to db
-    $.ajax({
-        type: 'POST',
-        data: newTote,
-        url: '/totes/createtote',
-        dataType: 'JSON' 
-    }).done(function( response ){
-        // Check for successful (blank) response
-        if (response.msg === '') {
-            // what to do if we did it successfully
-            window.location.href = "/";
-        }
-        else {
-            // If something goes wrong, alert the error message that our service returned
-            alert('Error: ' + response.msg);
-        }
-    });
-}
+var newBag;
 
 $(document).ready(function(){
-    updateTextAreaSize();
-    loadBags();
+    newBag = _.extend({}, bagObject);
+    newBag.data.editMode = true;
+    newBag.newTextField();
 
-    // scroll down
-    var scrollAmt = $(document).height() - $(window).height();
-    $(window).scrollTop(scrollAmt);
+    newBag.draw($(".content.create-page .bag-wrap"), function(){
+        $(".editable-field").eq(0).addClass("selected");
+        $(".editable-field").eq(0).find("textarea").focus();
 
-    // list of elements to ignore
-    $(document).hammer().on("tap", ".tool, .bag-size-wrap .icon", function(e){
+        // scroll down
+        setTimeout(function(){
+            var scrollAmt = $(document).height() - $(window).height();
+            $("body").animate({scrollTop : scrollAmt}, "slow");
+        }, 100);
+    });
+
+    // bag size toggling
+    $(document).hammer().on("tap", ".bag-size-wrap .icon", function(e){
+        e.preventDefault();
         e.stopPropagation();
+
+        if ($(this).hasClass("small-bag")){
+            newBag.toggleSize("small");
+        }
+        else {
+            newBag.toggleSize("big");
+        }
+     
+        $(this).siblings(".sel").removeClass("sel");
+        $(this).addClass("sel");
     });
 
-    // update textarea size on key entry
-    $(document).on("keyup", ".text-wrap textarea", function(){
-        updateTextAreaSize();
+    $(document).hammer().on("tap", ".addTextfield:not(.disabled)", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+
+        if ($(".editable-field.new").length > 0){
+            $(".editable-field").removeClass("selected");
+            setTimeout( function(){
+                $(".editable-field.new").eq(0).addClass("selected");
+            }, 100);
+        }
+        else{
+            newBag.newTextField();
+            newBag.draw();
+        }
     });
 
-    //selects tool on tap, deselects on tap out.
-    $(document).hammer().on("tap", ".tool", function(){
-        selectTool($(this));
-    });
-    $(document).hammer().on("tap", ".content", function(){
-        deselectAll();
+    $(document).hammer().on("tap", ".editable-field .corner.tr", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+
+        var id = $(this).parents(".editable-field").attr("data-id");
+        newBag.deleteTextField(id);
     });
 
-    //manages dragging only the text-wrap tool, for Y axis only.
-    $(document).hammer().on("drag", ".text-wrap", function(e){
-        if ( $(e.target).parents(".text-controls").length === 1 ){
+    // update the size of the text field in the UI as you type
+    $(document).on("keyup", ".editable-field textarea", function(){
+        newBag.updateTextAreaSize($(this));
+    });
+
+    // select a text field
+    $(document).hammer().on("tap", ".editable-field", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+
+        $(".editable-field").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    // when to deselect a text field
+    $(document).hammer().on("tap", ":not(.editable-field, .editable-field *)", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+
+        $(".editable-field.selected").removeClass("selected");
+    });
+
+    // using the justification controls.
+    $(document).hammer().on("tap", ".text-controls .justification > *", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        var $field = $(this).parents(".editable-field");
+        var tfData = _.findWhere(newBag.data.textfields, { "_id" : $field.attr("data-id") });
+        
+        $(this).siblings(".sel").removeClass("sel");
+
+        // set the alignment shiz        
+        var alignment = $(this).attr("class").split("-")[1];
+        tfData.justify = alignment;
+        $(this).parents(".editable-field").css("text-align", alignment);
+
+        // make it selected
+        $(this).addClass("sel");
+    });
+
+    // manages type control sliders
+    $(document).on("input", ".type-control input", function(){
+        typeSliderHandler($(this));
+    });
+    $(document).on("change", ".type-control input", function(){
+        typeSliderHandler($(this));
+    });
+
+    function typeSliderHandler($input){
+        var $field = $input.parents(".editable-field");
+        var tfData = _.findWhere(newBag.data.textfields, { "_id" : $field.attr("data-id") });
+        
+        if ($input.parent().hasClass("size")){
+            var newSize = $input.val();
+            $field.css("font-size", newSize + "em");
+
+            //font size to be saved
+            tfData.fontSize = Number(newSize);
+        }
+        else if ($input.parent().hasClass("kerning")){
+            var newKerning = $input.val();
+            $field.css("letter-spacing", newKerning + "em");
+
+            //kerning to be saved
+            tfData.kerning = Number(newKerning);
+        }
+        else if ($input.parent().hasClass("leading")){
+            var newLeading = $input.val();
+            $field.css("line-height", newLeading + "em");
+
+            //leading to be saved
+            tfData.leading = Number(newLeading);
+        }
+        newBag.updateTextAreaSize( $field.find("textarea") );
+    }
+
+    // //manages dragging only the text-wrap tool, for Y axis only.
+    $(document).hammer().on("drag", ".editable-field", function(e){
+        if ( $(e.target).parents(".text-controls").length === 1) {
             return;
         }
-
+        
         if (e.gesture.direction === "up" || e.gesture.direction === "down"){
             var dragDist = e.gesture.deltaY;
-            var amountMoved = Number($(this).attr("data-y"));
+            var index = $(this).attr("data-id");
+            var parentFontSize = parseInt($(this).parents(".bag-body").css("font-size"));
+
+            var tf = _.findWhere(newBag.data.textfields, { "_id" : index });
+            
+            // read it in pixels
+            var amountMoved = tf.y * parentFontSize;
 
             // if user is trying to drag above tote, cap at top of tote
             var dragAmt = dragDist + amountMoved;
             if (dragAmt < 0){
                 dragAmt = 0;
             }
-            else if ((dragAmt + $(".text-wrap").height()) > ($(".tote-wrap").height() * 0.4)){
-                dragAmt = ($(".tote-wrap").height() * 0.4) - $(".text-wrap").height();
+            else if ((dragAmt + $(this).height() > $(this).parents(".textfields-wrap").height()) ){
+                dragAmt = $(this).parents(".textfields-wrap").height() - $(this).height();
             }
-
-            TweenLite.to(this, 0, { y : dragAmt + "px" });
+            TweenLite.to($(this), 0, { y : dragAmt + "px" });
         }
         
     });
-    $(document).hammer().on("dragend", ".text-wrap", function(e){
-        if ( $(e.target).parents(".text-controls").length === 1 ){
+    $(document).hammer().on("dragend", ".editable-field", function(e){
+        if ( $(e.target).parents(".text-controls").length === 1) {
             return;
         }
 
         if (e.gesture.direction === "up" || e.gesture.direction === "down"){
             var dragDist = e.gesture.deltaY;
-            var amountMoved = Number($(this).attr("data-y"));
+            var index = $(this).attr("data-id");
+            var parentFontSize = parseInt($(this).parents(".bag-body").css("font-size"));
+
+            var tf = _.findWhere(newBag.data.textfields, { "_id" : index });
+            
+            // read it in pixels
+            var amountMoved = tf.y * parentFontSize;
 
             var dragAmt = dragDist + amountMoved;
-            if (dragAmt < 0){
+            if (dragAmt < 0) {
                 dragAmt = 0;
             }
-            else if ((dragAmt + $(".text-wrap").height()) > ($(".tote-wrap").height() * 0.4)){
-                dragAmt = ($(".tote-wrap").height() * 0.4) - $(".text-wrap").height();
+            else if ((dragAmt + $(this).height() > $(this).parents(".textfields-wrap").height()) ){
+                dragAmt = $(this).parents(".textfields-wrap").height() - $(this).height();
             }
 
-            $(this).attr("data-y", "" + dragAmt);
-            
-            // Y to be saved.
-            dataY = round(dragAmt, 4);
+            // save it in ems
+            tf.y = dragAmt / parentFontSize;
         }
-    });
-
-    // manages color of bag
-    $(document).hammer().on("tap", ".color-wrap", function(){
-        var selectedColor = $(this).attr("data-color");
-
-        if ($(".color-wrap.checked").attr("data-color") !== selectedColor){
-            $(".color-wrap.checked").removeClass("checked");
-            $(this).addClass("checked");
-
-            var currColor = $(".tote-wrap").attr("data-color");
-            $(".tote-wrap").removeClass(currColor);
-            $(".tote-wrap").addClass(selectedColor);
-            $(".tote-wrap").attr("data-color", selectedColor);
-
-            // color to be saved.
-            dataColor = $(".tote-wrap").attr("data-color");
-        }
-    });
-
-    // manages type control sliders
-    $(document).on("input", ".type-control input", function(){
-        if ($(this).parent().hasClass("size")){
-            var newSize = $(this).val() + "em";
-            $(".text-wrap, .clone-text").css("font-size", newSize);
-
-            //font size to be saved
-            dataFontSize = $(this).val();
-        }
-        else if ($(this).parent().hasClass("kerning")){
-            var newKerning = $(this).val() + "em";
-            $(".text-wrap, .clone-text").css("letter-spacing", newKerning);
-
-            //kerning to be saved
-            dataKerning = $(this).val();
-        }
-        else if ($(this).parent().hasClass("leading")){
-            var newLeading = $(this).val() + "em";
-            $(".text-wrap, .clone-text").css("line-height", newLeading);
-
-            //leading to be saved
-            dataLeading = $(this).val();
-        }
-        updateTextAreaSize();
-    });
-    // this function is only duplicated because earlier browsers
-    // don't support the "input" event but support "change".
-    $(document).on("change", ".type-control input", function(){
-        if ($(this).parent().hasClass("size")){
-            var newSize = $(this).val() + "em";
-            $(".text-wrap, .clone-text").css("font-size", newSize);
-
-            //font size to be saved
-            dataFontSize = $(this).val();
-        }
-        else if ($(this).parent().hasClass("kerning")){
-            var newKerning = $(this).val() + "em";
-            $(".text-wrap, .clone-text").css("letter-spacing", newKerning);
-
-            //kerning to be saved
-            dataKerning = $(this).val();
-        }
-        else if ($(this).parent().hasClass("leading")){
-            var newLeading = $(this).val() + "em";
-            $(".text-wrap, .clone-text").css("line-height", newLeading);
-
-            //leading to be saved
-            dataLeading = $(this).val();
-        }
-        updateTextAreaSize();
-    });
-
-    // manages type control's justification
-    $(document).hammer().on("tap", ".justification *", function(){
-        if ($(this).hasClass("sel")){
-            return;
-        }
-        $(".justification .sel").removeClass("sel");
-        $(this).addClass("sel");
-        
-        var toBeJust = $(this).attr("data-just");
-        $(".text-wrap textarea, .clone-text").css("text-align", toBeJust);
-
-        // Justification to be saved
-        dataJustify = toBeJust;
-
-        updateTextAreaSize();
-    });
-
-    // manages bag size control.
-    $(document).hammer().on("tap", ".bag-size-wrap .icon", function(){
-        if ($(this).hasClass("sel")){
-            return;
-        }
-
-        $(".bag-size-wrap .icon.sel").removeClass("sel");
-        $(this).addClass("sel");
-
-        if ($(this).attr("data-size") === "big"){
-            $(".tote-wrap").removeClass("small").addClass("big");
-        }
-        else{
-            $(".tote-wrap").removeClass("big").addClass("small");    
-        }
-
-        // Size to be saved.
-        dataSize = $(this).attr("data-size");
-        
-        updateTextAreaSize();
     });
 
     $(document).hammer().on("tap", "nav .right-actions .save", function(){
-        saveTote();
+        newBag.saveAs();
     });
 });
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,32 +1,10 @@
 //google analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+(i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-58975311-1', 'auto');
-
-//rounding method
-function round(num, places) {
-    var multiplier = Math.pow(10, places);
-    return Math.round(num * multiplier) / multiplier;
-}
-
-// Sort Json
-function sortByKey(array, key, direction) {
-    return array.sort(function(a, b) {
-        var x = a[key]; var y = b[key];
-        if (direction === "asc"){
-            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-        }
-        else if (direction === "desc"){
-            return ((x > y) ? -1 : ((x < y) ? 1 : 0));
-        }
-        else{
-            return;
-        }
-    });
-}
 
 // Likes API
 function userLikesToArray(){
@@ -64,6 +42,270 @@ function hasLike(toteId){
     return (toteIndex !== -1);
 }
 
+var site = {
+    colors : ["black", "white", "orange", "yellow", "cyan", "red", "green"],
+    chars : "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz".split(''),
+    randomString : function(length){
+        var string = '';
+        for (var i = 0; i < length; i++){
+            string += site.chars[ Math.floor(Math.random() * site.chars.length) ];
+        }
+        return string;
+    },
+    breakpts : {
+        sml : 650,
+        med : 960,
+        lrg : 1440
+    },
+    transistionEnd : 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+    breakpt : function() {
+        var ww = window.innerWidth;
+        var breakptval = 'lrg';
+
+        for( var key in site.breakpts){
+            if( site.breakpts[key] >= ww ){
+                breakptval = key;
+                break;
+            }
+        }
+        return breakptval;
+    },
+    refreshTypeOnTotes : function(){
+        _.each($(".actual-tote"), function(tote){
+            var width = $(tote).find(".bag-body").width();
+            var fontSize = width / 2;
+            $(tote).find(".bag-body").css("font-size", (fontSize + "px"));
+
+            if ( $(tote).find(".editable-field").length > 0 ){
+                var fields = $(tote).find(".textfields-wrap .editable-field");
+                for (var i = 0; i < fields.length; i++){
+                    bagObject.updateTextAreaSize( $(fields[i]).find("textarea") );
+                }
+            }
+            if ( $(tote).find(".text").length > 0 ){
+                var texts = $(tote).find(".textfields-wrap .text");
+                for (var j = 0; j < texts.length; j++){
+                    var newX = Number($(texts[j]).attr("data-x")) * fontSize;
+                    var newY = Number($(texts[j]).attr("data-y")) * fontSize;
+                    TweenLite.to(texts[j], 0, { x : newX, y : newY });
+                }
+            }
+        });
+    },
+    getTransformValue : function($el, orientation){
+        var matrix = $el.css('transform').replace(/[^0-9\-.,]/g, '').split(',');
+        var x = matrix[12] || matrix[4];
+        var y = matrix[13] || matrix[5];
+
+        if (x === undefined){
+            x = 0;
+        }
+        if (y === undefined){
+            y = 0;
+        }
+
+        if (orientation === "x"){
+            return x;
+        }
+        else if (orientation === "y"){
+            return y;
+        }
+    },
+    render : function(obj, tpl, target, onComplete) {
+        $.get("/templates/_"+tpl+".html", function(html) {
+            var template = Handlebars.compile(html);
+            var rendered = template(obj);
+
+            $(target).html(rendered);
+
+            if(typeof onComplete !== 'undefined'){
+                onComplete();
+            }
+        });
+    }
+};
+
+var bagObject = {
+    data : {
+                textfields : [],
+                size : "small",
+                color : "black",
+                likes : 0,
+                editMode : false
+            },
+    
+    htmlElement : null,
+    parent : null,
+
+    //refresh type based on bag size.
+    refreshType : function(){
+        var $theTote = this.htmlElement;
+        var width = $theTote.find(".bag-body").width();
+        var fontSize = width / 2;
+        $theTote.find(".bag-body").css("font-size", (fontSize + "px"));
+    },
+
+    //big to small, refresh type afterwards.
+    toggleSize : function(size){
+        var bag = this;
+
+        if (size === "big" || (bag.data.size === "small" && typeof size === "undefined")) {
+            bag.data.size = "big";
+            bag.htmlElement.removeClass("small").addClass("big");
+        }
+        else if (size === "small" || (bag.data.size === "big" && typeof size === "undefined") ){
+            bag.data.size = "small";
+            bag.htmlElement.removeClass("big").addClass("small");
+        }
+
+        setTimeout( function(){
+            bag.refreshType();
+        }, 300 );
+    },
+
+    // change color
+    changeColor : function(color){
+        var bag = this;
+
+        if (_.contains(site.colors, color)){
+            $(bag.htmlElement).attr("class", "tote-wrap " + color + " " + bag.data.size);
+            bag.data.color = color;
+        }
+    },
+
+    updateTextAreaSize : function($textarea){
+        var bag = this;
+
+        var $field = $textarea.parents(".editable-field");
+        var textFieldID = $field.attr("data-id");
+        var $clone = $textarea.siblings(".clone-text");
+        var content = $textarea.val();
+    
+        if (content === ""){
+            content = "Type Something.";
+        }
+
+        $clone.html(content.replace(/\n/g, '<br />'));
+
+        if (bag.data && bag.data.textfields){
+            var theTextField = _.findWhere(bag.data.textfields, { "_id" : textFieldID });
+            theTextField.text = content;
+
+            if ($textarea.val() === ""){
+                $field.addClass("new");
+            }
+            else{
+                $field.removeClass("new");
+            }
+
+            // dealing with x and y
+            var parentFontSize = parseInt($field.parents(".bag-body").css("font-size"))
+            var newX = theTextField.x * parentFontSize;
+            var newY = theTextField.y * parentFontSize;
+            TweenLite.to($field, 0, { x : newX, y : newY });
+        }
+        TweenLite.to($field, 0.1, { height : $clone.height() } );
+    },
+
+    draw : function($parent, onComplete){
+        var bag = this;
+
+        // if a $parent wasn't defined, and ones already assigned, use that one.
+        if (typeof $parent === "undefined" && bag.parent !== null){
+            $parent = $(bag.parent);
+        }
+
+        else if (bag.parent === null && typeof $parent === "undefined"){
+            console.log("you done fucked up");
+            return;
+        }
+        // if there is no parent assigned to the bag, the parameter is assigned.
+        else if (bag.parent === null && typeof $parent !== "undefined"){
+            bag.parent = $parent;
+        }
+
+        var viewObj = { bags : [bag.data]};
+
+        site.render(viewObj, "bag", bag.parent, function(){
+            bag.htmlElement = bag.parent.find(".tote-wrap");
+            site.refreshTypeOnTotes();
+
+            if (typeof onComplete !== "undefined"){
+                onComplete();
+            }
+        });
+    },
+    // right now this is function is only intended for the create page.
+    newTextField : function(){
+        var emptyTextObj = {
+            y : 0,
+            x : 0,
+            width : "100%",
+            justify : "left",
+            fontSize : 0.28,
+            kerning : -0.03,
+            leading : 1
+        };
+        var id = site.randomString(16);
+        // if it finds something (not undefined), choose another id.
+        while ( typeof _.findWhere(this.data.textfields, {"_id" : id }) !== "undefined" ){
+            id = site.randomString(16);
+        }
+        emptyTextObj._id = id;
+
+        this.data.textfields.push(emptyTextObj);
+
+        if (this.data.textfields.length >= 4){
+            $(".addTextfield").addClass("disabled");
+        }
+    },
+    deleteTextField : function(id, callback){
+        var bag = this;
+        var $field = $(this.htmlElement).find(".editable-field[data-id=" + id + "]");
+        TweenLite.to($field, 0.3, {
+            scale : 1.15,
+            alpha : 0,
+            onComplete : function(){
+                $field.remove();
+                bag.data.textfields = _.reject(bag.data.textfields, function(tf){
+                    return tf._id === id;
+                });
+                if (bag.data.textfields.length < 4){
+                    $(".addTextfield").removeClass("disabled");
+                }
+
+                if (typeof callback !== "undefined"){
+                    callback();
+                }
+            }
+        });
+    },
+    saveAs : function(){
+        var bagData = _.extend({}, this.data);
+        bagData.textfields = JSON.stringify(bagData.textfields);
+        bagData.editMode = null;
+        bagData.timestamp = Math.round(new Date().getTime() / 1000);
+        
+        // use ajax to post tote to db
+        $.ajax({
+            type: 'POST',
+            data: bagData,
+            url: '/totes/createtote',
+            dataType: 'JSON' 
+        }).done(function( response ){
+            // Check for successful (blank) response
+            if (response.msg === '') {
+                // what to do if we did it successfully
+                window.location.href = "/";
+            }
+            else {
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+            }
+        });
+    }
+};
+
 $(document).ready(function(){
     if ($.cookie("likes") === undefined){
         $.cookie("likes", "");
@@ -71,6 +313,11 @@ $(document).ready(function(){
 
     $(document).hammer().on("tap", ".logo", function(){
         window.location.href = "/";
+    });
+
+    site.refreshTypeOnTotes();
+    $(window).resize(function(){
+        site.refreshTypeOnTotes();
     });
 
     ga('send', 'pageview');
